64c1828dfa5a06114a1930d2d29a579a595cdc7f
Toan <toan.dev@outlook.com>
2025-02-07 19:09:04 -0300

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}" // Inclui todos os arquivos HTML e TypeScript no projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
