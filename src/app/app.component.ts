5cb67431f4024ad0a939168d9870e4b8f5883e38
Toan <toan.dev@outlook.com>
2025-02-09 13:16:01 -0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WhatsappButtonComponent } from './ui/whatsapp-button/whatsapp-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [WhatsappButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'escmaia-portifolio';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{html,ts}" // Inclui todos os arquivos HTML e TypeScript no projeto
+    "./src/**/*.{html,ts}", // Inclui todos os arquivos HTML e TypeScript no projeto
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["roboto", "serif"],
+        skranji: ["Skranji", "serif"],
+      },
+
+      colors: {
+        whatsapp: "#3d9a73",
+      },
+    },
   },
   plugins: [],
 };
